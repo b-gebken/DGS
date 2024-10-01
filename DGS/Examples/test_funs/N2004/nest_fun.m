644f9a848d7bef778acdd536b10d482d2ca8e32e
b-gebken <115168210+b-gebken@users.noreply.github.com>
2024-10-01 11:19:04 +0200

    Delete DGS directory

--- DGS/Examples/test_funs/N2004/nest_fun.m
+++
@@ -1,7 +0,0 @@
-function [val,I] = nest_fun(x,m)
-
-[max_val,I] = max(x(1:m,:),[],1);
-val = max_val + 1/2*vecnorm(x,2,1).^2;
-
-end
-
--- DGS/Examples/test_funs/p_order_growth_fun/p_order_growth_fun.m
+++
@@ -1,9 +0,0 @@
-function y = Ex6(x,p)
-
-% global x_arr_glob
-% x_arr_glob = [x_arr_glob,x];
-
-y = max([1/p*abs(x(1,:)).^p;abs(x(2,:))],[],1);
-
-end
-
