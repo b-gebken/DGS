3dc84a128d5f6277fc871f59a5d95727ccdb5d5c
Bennet <115168210+b-gebken@users.noreply.github.com>
2026-06-23 14:50:24 +0200

    Update descent_direction.m
    In the old version, if rand_sample_N > 0 and memory.max_size > 0, then all randomly sampled points would occur twice in the bundle W. This is now fixed. (This has no implications for the paper results, as the above settings were not used there.)

--- descent_direction.m
+++ descent_direction.m
@@ -4,29 +4,32 @@
 
 function [v,f_eps_v,memory,eval_counter] = descent_direction(x,f_x,f,subgrad_f,eps,delta,c,rand_sample_N,memory,qp_optns,eval_counter)
 
+W = [];
+
+% Add subgradients at sample points in B_eps(x) from memory
+if(memory.max_size > 0 && ~isempty(memory.sample_pts))
+    inside_Beps = vecnorm(memory.sample_pts - x,2,1) <= eps + 10^-14; % Tolerance for machine precision
+    W = [W,memory.subgrads(:,inside_Beps)];
+end
+
 if(rand_sample_N == 0)
     % Step 1 in [GP2021] (Deterministic initial approximation)
-    W = subgrad_f(x); eval_counter.subgrad_eval = eval_counter.subgrad_eval + 1;
+    W = [W,subgrad_f(x)]; eval_counter.subgrad_eval = eval_counter.subgrad_eval + 1;
     if(memory.max_size > 0)
         memory = add_to_memory(x,W,memory);
     end
 else
     % Random initial approximation
     n = size(x,1);
-    W = zeros(n,rand_sample_N);
+    W_rnd = zeros(n,rand_sample_N);
     sample_pts = x + eps*sample_hypersphere(n,rand_sample_N);
     for i = 1:rand_sample_N
-        W(:,i) = subgrad_f(sample_pts(:,i)); eval_counter.subgrad_eval = eval_counter.subgrad_eval + 1;
+        W_rnd(:,i) = subgrad_f(sample_pts(:,i)); eval_counter.subgrad_eval = eval_counter.subgrad_eval + 1;
     end
+    W = [W,W_rnd];
     if(memory.max_size > 0)
-        memory = add_to_memory(sample_pts,W,memory);
+        memory = add_to_memory(sample_pts,W_rnd,memory);
     end
-end
-
-% Add subgradients at sample points in B_eps(x) from memory
-if(memory.max_size > 0 && ~isempty(memory.sample_pts))
-    inside_Beps = vecnorm(memory.sample_pts - x,2,1) <= eps + 10^-14; % Tolerance for machine precision
-    W = [W,memory.subgrads(:,inside_Beps)];
 end
 
 while(1)
